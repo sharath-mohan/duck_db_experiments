423689bb951f2b68e1f00d5d62cfc4fb8a22e1dc
sharath-mohan <sharathdesignsofficial@gmail.com>
2026-01-21 23:33:08 +0530

    feat: duckdb based python app added

--- python-app/main.py
+++ python-app/main.py
@@ -1,5 +1,23 @@
+import duckdb
+
+class DuckDB:
+    def __init__(self):
+        self.con = duckdb.connect(database=":memory:", read_only=False)
+    def install_httpfs(self):
+        print("Installing httpfs...")
+        self.con.execute("INSTALL httpfs;")
+        self.con.execute("LOAD httpfs;")
+        print("httpfs installed successfully")
+    def read_parquet(self, path: str):
+        return self.con.execute(f"SELECT * FROM read_parquet('{path}');").fetchone()
+
+
 def main():
-    print("Hello from python-app!")
+    db = DuckDB()
+    db.install_httpfs()
+    data = db.read_parquet("http://localhost:3000/get-data")
+    print(data)
+    
 
 
 if __name__ == "__main__":
